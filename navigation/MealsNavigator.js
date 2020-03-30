1c8a62eead77dc695552d5eb2dc343864ef35816
Pablo Tabares H <pablother3@gmail.com>
2020-03-29 21:18:08 -0500

    navigation tab for android

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createStackNavigator, createBottomTabNavigator, createAppContainer } from 'react-navigation'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 import { Ionicons } from '@expo/vector-icons'
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
@@ -7,6 +8,16 @@ import MealDetailScreen from '../screens/MealDetailScreen'
 import Color from '../constants/Color';
 import { Platform } from 'react-native'
 import FavoritesScreen from '../screens/FavoritesScreen';
+
+const defaultStackNavOptions = {
+  //headerMode: 'modal',
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Color.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Color.primaryColor
+  }
+}
 
 const MealsNavigator = createStackNavigator({
   Categories: CategoriesScreen,
@@ -21,24 +32,21 @@ const MealsNavigator = createStackNavigator({
   },
   MealDetail: MealDetailScreen,
 
-},
-{
-  //headerMode: 'modal',
-  defaultNavigationOptions: {
-    headerStyle: {
-      backgroundColor: Platform.OS === 'android' ? Color.primaryColor : ''
-    },
-    headerTintColor: Platform.OS === 'android' ? 'white' : Color.primaryColor
-  }
-})
+}, defaultStackNavOptions)
 
-const MealsFavTabNavigator = createBottomTabNavigator({
+const FavNavigatior = createStackNavigator({
+  Favorites: FavoritesScreen,
+  MealDetail: MealDetailScreen
+}, defaultStackNavOptions)
+
+const tabScreenConfig = {
   Meals: {
     screen: MealsNavigator,
     navigationOptions: {
       tabBarIcon: (tabInfo) => {
         return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />
-      }
+      },
+      tabBarColor: Color.primaryColor // Change color of tab
     }
   },
   Favorites: {
@@ -47,10 +55,22 @@ const MealsFavTabNavigator = createBottomTabNavigator({
       tabBarLabel: 'Favorites!',
       tabBarIcon: (tabInfo) => {
         return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
-      }
+      },
+      tabBarColor: Color.accentColor // Different color tab
     }
   }
-}, {
+}
+
+const MealsFavTabNavigator = Platform.OS === 'android'
+? createMaterialBottomTabNavigator(tabScreenConfig, {
+  activeColor: 'white',
+  shifting: false, // Nice animation tab transition and change color
+  barStyle: {
+    backgroundColor: Color.primaryColor // Set color bar style
+  }
+})
+: createBottomTabNavigator(
+  tabScreenConfig, {
   tabBarOptions: {
     activeTintColor: Color.accentColor
   }
